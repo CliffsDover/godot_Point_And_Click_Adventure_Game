b61295d540a2602248071c750d96fc9fc51611d6
CliffsDover <cliffs@dancingbottle.com>
2018-05-17 14:01:13 +0800

    add global Game object

---
+++ scripts/Game.gd
@@ -0,0 +1,6 @@
+extends "res://templates/scripts/Game_Base.gd"
+
+var verbs = [ "Look", "Use", "Take", "Open", "Close" ]
+
+func GetAllVerbs():
+	return verbs
--- templates/scenes/Scene_Base.gd
+++ templates/scenes/Scene_Base.gd
@@ -3,7 +3,7 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-
+onready var Game = get_node("/root/Game" )
 onready var Cursor = preload( "res://templates/scenes/Cursor_Crosshair.tscn" ).instance()
 
 func _ready():
@@ -11,6 +11,7 @@ func _ready():
 	set_process( true )
 	Input.set_mouse_mode( Input.MOUSE_MODE_HIDDEN )
 	add_child( Cursor )
+	print( Game.GetAllVerbs() )
 
 func _process(delta):
 	$Cursor.position = get_global_mouse_position()
