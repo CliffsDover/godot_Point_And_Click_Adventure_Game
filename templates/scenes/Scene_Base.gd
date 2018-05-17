c6b484b50eca9f2a24cd95c96a78a9b1af72cd1b
CliffsDover <cliffs@dancingbottle.com>
2018-05-17 13:36:39 +0800

    Add Cursor_Base and Scene_Base

---
+++ templates/scenes/Scene_Base.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+onready var Cursor = preload( "res://templates/scenes/Cursor_Crosshair.tscn" ).instance()
+
+func _ready():
+	set_process_input( true )
+	set_process( true )
+	Input.set_mouse_mode( Input.MOUSE_MODE_HIDDEN )
+	add_child( Cursor )
+
+func _process(delta):
+	$Cursor.position = get_global_mouse_position()
