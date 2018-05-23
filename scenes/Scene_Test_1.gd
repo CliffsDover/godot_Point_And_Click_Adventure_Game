70f34878ffc99cbc8f049ebaca8f50a3e2ea9841
CliffsDover <bottle@dancingbottle.com>
2018-05-23 09:24:39 +0800

    WIP

--- scenes/Scene_Test_1.gd
+++ scenes/Scene_Test_1.gd
@@ -11,7 +11,7 @@ var path = []
 func _ready():
 	#ShowInfoBar( false )
 	SetInfoText( "Scene 1" )
-	pass
+	$Objects/Player.SetNavigation2D( $Backgrounds/Navigation2D )
 	#print( "[Scene_Test_1] _ready")
 	#print( Game.GetAllVerbs() )
 	#set_process_input( true )
@@ -23,34 +23,35 @@ func ActionHandler( objects ):
 
 
 func CursorClickedAt( pos ):
-	print( "[CursorClickedAt]" )
-	print( pos )
+	#print( "[CursorClickedAt]" )
+	#print( pos )
 	#updatePath( $Objects/Player.position, get_global_mouse_position() )
+	$Objects/Player.WalkTo( pos )
 
 	
 func updatePath( startPos, endPos ):
-	#path = $Backgrounds/Navigation2D.get_simple_path( startPos, endPos, false )
-	#if path.size() == 0:
-	#update()	
-	pass	
+	path = $Backgrounds/Navigation2D.get_simple_path( startPos, endPos, true )
+	if path.size() > 0:
+		update()	
+		
 
 
 func _process(delta):
 	if path.size() >= 1:
-		print( "-------" )
-		print( $Objects/Player.position )
-		print( path[0] )
+		#print( "-------" )
+		#print( $Objects/Player.position )
+		#print( path[0] )
 		
 		var d = $Objects/Player.position.distance_to( path[0] )
 		if d > 2:
 			$Objects/Player.position = $Objects/Player.position.linear_interpolate( path[0], ( 200 * delta ) / d )
 			$Objects/Player/RayCast2D.look_at( path[0] )
-			print( $Objects/Player/RayCast2D.get_angle_to( Vector2(1,0) ) ) 
+			#print( $Objects/Player/RayCast2D.get_angle_to( Vector2(1,0) ) ) 
 		else:
 			path.remove( 0 ) 
 
 		
 func _draw():
 	for p in path:
-		draw_circle( p, 10, Color(1,0,0,1) )
+		draw_circle( p, 3, Color(1,0,0,1) )
 		
